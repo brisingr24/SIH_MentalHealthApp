ffa40fe64007656c5fc60d6f25ab37d7bb200c67
brisingr24 <rawatadity234@gmail.com>
2022-05-19 07:17:57 +0530

    Log in and Registration

--- lib/register.dart
+++ lib/register.dart
@@ -1,6 +1,11 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+import 'dart:io';
 import 'home.dart';
 import 'login.dart';
 
@@ -16,12 +21,14 @@ class _RegisterPageState extends State<RegisterPage> {
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   var mailController = TextEditingController();
   var passController = TextEditingController();
+  UploadTask? task;
+  File? file;
 
   checkAuth() async {
     _auth.authStateChanges().listen((User? user) {
       if (user != null) {
         Navigator.push(
-          context,
+          this.context,
           MaterialPageRoute(builder: (context) => Home()),
         );
       } else {
@@ -49,13 +56,39 @@ class _RegisterPageState extends State<RegisterPage> {
     }
   }
 
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+    final path = result.files.single.path!;
+
+    setState(() {
+      file = File(path);
+    });
+  }
+
+  Future uploadFile() async {
+    if (file == null) return;
+    final filename = basename(file!.path);
+    final destination = 'files/$filename';
+
+    FirebaseApi.uploadFile(destination,file!);
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    final fileName = file !=null? basename(file!.path) :'No File Selected';
     return Scaffold(
       body: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              height: 200,
+              width: 200,
+              child: const Image(image: AssetImage("images/KIRAN.png"),),
+            ),
+            SizedBox(height: 30,),
             Container(
               child: Form(
                 key: _formkey,
@@ -86,6 +119,20 @@ class _RegisterPageState extends State<RegisterPage> {
                       // onSaved:
                       // (input) => email = input!,
                     ),
+                    SizedBox(height: 30,),
+                    RaisedButton(
+                        child: Text("Select File"),
+                        onPressed: () {
+                          selectFile();
+                        }),
+                    Text(fileName,style: TextStyle(fontSize: 16),),
+                    SizedBox(height: 30,),
+                    RaisedButton(
+                        child: Text("Upload File"),
+                        onPressed: () {
+                          uploadFile();
+                        }),
+                    SizedBox(height: 60,),
                     RaisedButton(
                         child: Text("REGISTER"),
                         onPressed: () {
@@ -105,5 +152,17 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
       ),
     );
+  }
+}
+
+class FirebaseApi {
+  static UploadTask? uploadFile(String destination,File file){
+
+    try {
+      final ref = FirebaseStorage.instance.ref(destination);
+      return ref.putFile(file);
+    } on FirebaseException catch(e){
+      return null;
+    }
   }
 }
